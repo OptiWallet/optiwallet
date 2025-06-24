4def66ceab8f2bb1c08335a893297737b499b09c
eviatarHasid <evi1020@gmail.com>
2025-06-24 17:13:50 -0400

    adding filter component, folder layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,45 @@ import { TextField, TextFieldRoot } from "@/components/ui/textfield";
 import { createSignal } from "solid-js";
 import { NavBar } from "./components/navbar";
 import { Card, CardContent } from "./components/ui/card";
-  
+import { Filter } from "./components/filter/filter.component";
+
+const creditScoreOptions: string[] = ["100", "200", "300", "400", "500"];
+
+const incomeOptions: string[] = ["100", "200", "300", "400", "500"];
+
 export const App = () => {
-  let nameField: HTMLInputElement |    undefined;
-  const [name, setName] = createSignal<string>('Guest');
+  let nameField: HTMLInputElement | undefined;
+  const [name, setName] = createSignal<string>("Guest");
+  const [creditScore, setCreditScore] = createSignal<string>();
+  const [income, setIncome] = createSignal<string>();
+
   return (
     <>
-      <NavBar/>
-      <div class='mx-auto max-w-5xl h-screen p-4 flex flex-col gap-3 bg-zinc-100 dark:bg-zinc-900'>
+      <NavBar />
+      <div class="mx-auto max-w-5xl h-screen p-4 flex flex-col gap-3 bg-zinc-100 dark:bg-zinc-900">
+        <Card>
+          <CardContent class="flex flex-row gap-4 items-center">
+            <Filter
+              options={creditScoreOptions}
+              placeholder="Credit Score"
+              onChange={setCreditScore}
+            />
+            <Filter
+              options={incomeOptions}
+              placeholder="Income"
+              onChange={setIncome}
+            />
+          </CardContent>
+        </Card>
         <Card>
           <CardContent>
             Welcome {name()}!
             <TextFieldRoot>
               <TextField placeholder="Enter your name" ref={nameField} />
             </TextFieldRoot>
-            <Button on:click={() => setName(nameField?.value ?? '')}>Submit</Button>
+            <Button on:click={() => setName(nameField?.value ?? "")}>
+              Submit
+            </Button>
           </CardContent>
         </Card>
       </div>
