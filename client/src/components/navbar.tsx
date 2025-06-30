cc9fc92eae3b6a901c4a51927f44e654e444d671
evi-h <evi1020@gmail.com>
2025-06-30 13:25:54 -0400

    adding mobile navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,80 +1,165 @@
 import logo from "@/assets/logo.svg";
 import theme from "@/assets/theme.svg";
 import { NavigationMenu } from "@kobalte/core/navigation-menu";
-import { NavigationMenuItem, NavigationMenuTrigger } from "./ui/navigation-menu";
+import {
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+import { DropdownMenu } from "@kobalte/core/dropdown-menu";
+import { createSignal, onCleanup, onMount } from "solid-js";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useNavigate } from "@solidjs/router";
+import { FiMenu } from "solid-icons/fi";
+import { IoSettingsOutline } from "solid-icons/io";
 
 export interface NavBarProps {
-    loggedIn?: boolean;
+  loggedIn?: boolean;
 }
 
-export const NavBar = (props: NavBarProps) => {
-    const {loggedIn = false} = props;
+const NavBarLargeScreen = (props: NavBarProps) => {
+  const { loggedIn = false } = props;
 
-    return (
-      <>
-        <div class="flex justify-between bg-background sticky top-0 px-2 drop-shadow-md">
-          <NavigationMenu class={"flex items-center h-14"}>
-            <NavigationMenuItem>
+  return (
+    <>
+      <div class="flex justify-between bg-background sticky top-0 px-2 drop-shadow-md">
+        <NavigationMenu class={"flex items-center h-14"}>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              withArrow={false}
+              as="a"
+              href="/"
+              imgSrc={logo}
+              class="h-11 w-11 p-0 mt-1"
+            ></NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger withArrow={false} as="a" href="/creditcards">
+              Credit Cards
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              withArrow={false}
+              as="a"
+              href="/bankaccounts"
+            >
+              Bank Accounts
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenu>
+        <NavigationMenu class={"flex items-center h-14 gap-2"}>
+          <NavigationMenuItem>
+            {loggedIn ? (
               <NavigationMenuTrigger
                 withArrow={false}
                 as="a"
-                href="/"
-                imgSrc={logo}
-                class="h-11 w-11 p-0 mt-1"
-              ></NavigationMenuTrigger>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger
-                withArrow={false}
-                as="a"
-                href="/creditcards"
+                href="/profile"
+                class="h-8 w-8 p-1"
               >
-                Credit Cards
+                Profile
               </NavigationMenuTrigger>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
+            ) : (
               <NavigationMenuTrigger
                 withArrow={false}
                 as="a"
-                href="/bankaccounts"
+                href="/login"
+                class="h-8 w-8 p-1"
               >
-                Bank Accounts
+                Login
               </NavigationMenuTrigger>
-            </NavigationMenuItem>
-          </NavigationMenu>
-          <NavigationMenu class={"flex items-center h-14 gap-2"}>
-            <NavigationMenuItem>
+            )}
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              withArrow={false}
+              as="a"
+              href="/darkmode"
+              imgSrc={theme}
+              class="h-5 w-5 p-0 mt-1"
+            ></NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenu>
+      </div>
+    </>
+  );
+};
+
+const NavBarMobile = (props: NavBarProps) => {
+  const { loggedIn = false } = props;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div class="flex justify-between bg-background sticky top-0 px-2 drop-shadow-md">
+        {/* Left: burger menu */}
+        <DropdownMenu>
+          <DropdownMenuTrigger class="h-11 w-11 p-0 mt-1">
+            <FiMenu />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onSelect={() => navigate("/")}>
+              Home
+            </DropdownMenuItem>
+            <DropdownMenuItem onSelect={() => navigate("/creditcards")}>
+              Credit Cards
+            </DropdownMenuItem>
+            <DropdownMenuItem onSelect={() => navigate("/bankaccounts")}>
+              Bank Accounts
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* Right: settings and dark mode */}
+        <div class="flex flex-row items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger class="h-11 w-11 flex mt-1 items-center justify-center">
+              <IoSettingsOutline />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
               {loggedIn ? (
-                <NavigationMenuTrigger
-                  withArrow={false}
-                  as="a"
-                  href="/profile"
-                  class="h-8 w-8 p-1"
-                >
+                <DropdownMenuItem onSelect={() => navigate("/profile")}>
                   Profile
-                </NavigationMenuTrigger>
+                </DropdownMenuItem>
               ) : (
-                <NavigationMenuTrigger
-                  withArrow={false}
-                  as="a"
-                  href="/login"
-                  class="h-8 w-8 p-1"
-                >
+                <DropdownMenuItem onSelect={() => navigate("/login")}>
                   Login
-                </NavigationMenuTrigger>
+                </DropdownMenuItem>
               )}
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger
-                withArrow={false}
-                as="a"
-                href="/darkmode"
-                imgSrc={theme}
-                class="h-5 w-5 p-0 mt-1"
-              ></NavigationMenuTrigger>
-            </NavigationMenuItem>
-          </NavigationMenu>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <button
+            class="h-11 w-11 p-0 mt-1 flex items-center justify-center"
+            onClick={() => navigate("/darkmode")}
+          >
+            <img src={theme} alt="Dark mode" class="h-5 w-5" />
+          </button>
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
+};
+
+export const NavBar = (props: NavBarProps) => {
+  const { loggedIn = false } = props;
+  const [isMobile, setIsMobile] = createSignal(false);
+
+  onMount(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth < 768);
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    onCleanup(() => window.removeEventListener("resize", checkMobile));
+  });
+
+  return (
+    <>
+      {isMobile() ? (
+        <NavBarMobile loggedIn={loggedIn} />
+      ) : (
+        <NavBarLargeScreen loggedIn={loggedIn} />
+      )}
+    </>
+  );
 };
