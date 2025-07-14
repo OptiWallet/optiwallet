b8c620555f9ea7eaa9ef866cf83b004183b000c6
Linx <amitoh@gmail.com>
2025-07-13 23:55:22 -0700

    Add db client and basic routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from "elysia";
+import { creditCardsRoutes } from "./routes/creditCards";
+import swagger from "@elysiajs/swagger";
+import { helmet } from "elysia-helmet";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(helmet());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(swagger());
+}
+
+app
+  .group("/api", (api) => api.use(creditCardsRoutes))
+  .listen(process.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
